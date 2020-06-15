f62ef04c9ff27b468969e993cd0157dd7296f42b
Art <ya.Artriches@ya.ru>
2020-06-15 22:59:05 +0300

    add: настраивает Routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,10 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import QuestionArtistScreen from '../question-artist/question-artist.jsx';
+import QuestionGenreScreen from '../question-genre/question-genre.jsx';
 
 class App extends PureComponent {
   constructor(props) {
@@ -12,10 +15,22 @@ class App extends PureComponent {
     const {errorsCount} = this.props;
 
     return (
-      <WelcomeScreen
-        errorsCount = {errorsCount}
-        onWelcomeButtonClick = {() => {}}
-      />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen
+              errorsCount = {errorsCount}
+              onWelcomeButtonClick = {() => {}}
+            />
+          </Route>
+          <Route exact path="/dev-artist">
+            <QuestionArtistScreen />
+          </Route>
+          <Route exact path="/dev-genre">
+            <QuestionGenreScreen />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
