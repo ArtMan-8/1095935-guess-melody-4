9f7f12b65c80f410f5a3b0939e249cace0762333
Art <ya.Artriches@ya.ru>
2020-06-24 11:54:56 +0300

    fix: моки - добавляет разных мелодий

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -8,20 +8,20 @@ export default [
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rock`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/d5/Chromatic_Fantasia_%28Bach_BWV_930%29.ogg`,
       genre: `blues`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/0/04/French-suite-5-courante-BWV_816.ogg`,
       genre: `jazz`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/46/Wo_soll_ich_fliehen_hin%2C_BWV_5.ogg`,
       genre: `rock`,
     }],
   }, {
     type: `artist`,
     song: {
       artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1d/Prelude_en_Fa_Majeur_BWV_927.ogg`,
     },
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
