80981896b7abaac3102bba47b16f89deb5587d92
Art <ya.Artriches@ya.ru>
2020-06-28 15:32:09 +0300

    add: подключает reducer в index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/app.jsx';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
+import {reducer} from './reducer.js';
+import App from './components/app/app.jsx';
 import questions from './mocks/questions.js';
+
+const store = createStore(reducer);
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
 ReactDOM.render(
-    <App
-      errorsCount = {Settings.ERRORS_COUNT}
-      questions = {questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount = {Settings.ERRORS_COUNT}
+        questions = {questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
