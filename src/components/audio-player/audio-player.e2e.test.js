961935bd9b83b80eb37348897a6a5759d42b3351
Art <ya.Artriches@ya.ru>
2020-07-02 19:02:58 +0300

    add: добовляет e2e тесты на плеер

--- src/components/audio-player/audio-player.e2e.test.js
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, {mount} from 'enzyme';
+import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import AudioPlayer from './audio-player.jsx';
 
@@ -7,52 +7,18 @@ Enzyme.configure({
   adapter: new Adapter(),
 });
 
-const mock = {
-  song: {
-    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
-  }
-};
-
 describe(`AudioPlayer`, () => {
-  const {song} = mock;
-  const handlePlayButtonClick = jest.fn();
-  const playButton = `.track__button`;
-  const playButtonOff = `.track__button--pause`;
-  const playButtonOn = `.track__button--play`;
-
-  it(`is play`, () => {
-    const screen = mount(
+  it(`Click by Play button calls callback`, () => {
+    const handlePlayButtonClick = jest.fn();
+    const wrapper = shallow(
         <AudioPlayer
-          src = {song.src}
-          isPlaying = {true}
-          isLoading={true}
-          onPlayButtonClick = {handlePlayButtonClick}
-        >
+          isLoading={false}
+          isPlaying={false}
+          onPlayButtonClick={handlePlayButtonClick}>
           <audio />
-        </AudioPlayer>
-    );
+        </AudioPlayer>);
 
-    const button = screen.find(playButton);
-    button.prop(handlePlayButtonClick());
-    expect(handlePlayButtonClick).toHaveBeenCalled();
-    expect(button.is(playButtonOff)).toEqual(true);
-  });
-
-  it(`is pause`, () => {
-    const screen = mount(
-        <AudioPlayer
-          src = {song.src}
-          isPlaying = {false}
-          isLoading={true}
-          onPlayButtonClick = {handlePlayButtonClick}
-        >
-          <audio />
-        </AudioPlayer>
-    );
-
-    const button = screen.find(playButton);
-    button.prop(handlePlayButtonClick());
-    expect(handlePlayButtonClick).toHaveBeenCalled();
-    expect(button.is(playButtonOn)).toEqual(true);
+    wrapper.find(`.track__button`).simulate(`click`);
+    expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
   });
 });
