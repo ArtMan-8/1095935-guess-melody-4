44b3770692b6302b05e8ac7d1fa11d8bf7f9f82a
Art <ya.Artriches@ya.ru>
2020-07-08 11:35:39 +0300

    add: api

---
+++ src/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const Error = {
+  UNAUTHORIZED: 401
+};
+
+export const createAPI = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-3.appspot.com/guess-melody`,
+    timeout: 5000,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => response;
+
+  const onError = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnauthorized();
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onError);
+  return api;
+};
